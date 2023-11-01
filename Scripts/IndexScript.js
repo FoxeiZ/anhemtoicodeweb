79d80a60aab8e052dd7c20842de5b3c802bc7a89
FoxeiZ <foxeiz2004@gmail.com>
2023-11-01 13:27:43 +0700

    cleanup script

--- Scripts/IndexScript.js
+++ Scripts/IndexScript.js
@@ -4,16 +4,18 @@
         autoplaySpeed: 3000,
         prevArrow: "#slide1 .slick-prev",
         nextArrow: "#slide1 .slick-next"
-
     });
-});
 
-$(document).ready(function () {
     $('#slide2 .slide-container').slick({
         autoplay: true,
         autoplaySpeed: 2000,
         prevArrow: "#slide2 .slick-prev",
         nextArrow: "#slide2 .slick-next"
-
     });
+
+    //$("div.category").slick({
+    //    lazyLoad: 'ondemand',
+    //    slidesToShow: 6,
+    //    slidesToScroll: 1
+    //});
 });
