ca64dfa53daaded4ccafa00dcf8c32ef6c41e25a
FoxeiZ <foxeiz2004@gmail.com>
2023-11-06 18:17:03 +0700

    fix type

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -14,8 +14,8 @@ namespace anhemtoicodeweb.Controllers
         private Model1 database = new Model1();
         public ActionResult Index()
         {
-            IEnumerable<Product> productList = database.Products.OrderByDescending(x => x.NamePro);
-            IEnumerable<Category> categoriesList = database.Categories.OrderByDescending(x => x.NameCate);
+            IEnumerable<Product> productList = database.Products.OrderByDescending(x => x.NamePro).ToList();
+            IEnumerable<Category> categoriesList = database.Categories.OrderByDescending(x => x.NameCate).ToList();
             var tuple = new Tuple<IEnumerable<Product>, IEnumerable<Category>>(productList, categoriesList);
             return View(productList);
         }
